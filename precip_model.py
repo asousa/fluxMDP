6f26fbd8447ff5cc158a0b1c01d86e8b0cd330d8
Austin Sousa <austin@rigby.stanford.edu>
2015-11-19 23:47:09 -0800

    Added per-energy interpolators

--- precip_model.py
+++ precip_model.py
@@ -13,7 +13,7 @@ import geopy.distance
 
 
 class precip_model(object):
-    def __init__(self,database="database.pkl", energies=False):
+    def __init__(self,database="database.pkl", multiple_bands=False):
         self.R_earth = geopy.distance.EARTH_RADIUS
         self.d2r = np.pi/180.0
         self.path_atten = -12 # db per 1000 km attenuation (approximation of decay for earth-ionsphere waveguide)
@@ -22,27 +22,40 @@ class precip_model(object):
             self.db = pickle.load(file)
 
         in_lats = sorted(self.db.keys())
+        self.multiple_bands = multiple_bands
 
+        # Simple dataset using total flux -- interpolates input latitude, output latitude, time.
         N = []
         S = []
-        if not energies:
-            # Simple dataset using total flux -- interpolates input latitude, output latitude, time.
-
+        for i in in_lats:
+            obj = self.db[i]
+            N.append(obj.N)
+            S.append(obj.S)
+        print np.shape(N)
+        N = np.array(N).swapaxes(1,2)
+        S = np.array(S).swapaxes(1,2)
+        self.I0 = obj.I0
+        print np.shape(N)
+        self.N_interp = interpolate.RegularGridInterpolator((in_lats, obj.coords.lat(), obj.t), N, fill_value=0,bounds_error=False)
+        self.S_interp = interpolate.RegularGridInterpolator((in_lats, obj.coords.lat(), obj.t), S, fill_value=0,bounds_error=False)
+    
+        if multiple_bands:
+            # Split into multiple bands -- input latitude, output latitude, energy, time.
+            N_E = []
+            S_E = []
             for i in in_lats:
-                obj = self.db[i]e
-                N.append(obj.N)
-                S.append(obj.S)
+                obj = self.db[i]
+                N_E.append(obj.N_E)
+                S_E.append(obj.S_E)
 
-            N = np.array(N).swapaxes(1,2)
-            S = np.array(S).swapaxes(1,2)
-            self.I0 = obj.I0
-            
-            self.N_interp = interpolate.RegularGridInterpolator((in_lats, obj.coords.lat(), obj.t), N, fill_value=0,bounds_error=False)
-            self.S_interp = interpolate.RegularGridInterpolator((in_lats, obj.coords.lat(), obj.t), S, fill_value=0,bounds_error=False)
-        
-        else:
-            # More-complicated: Interpolates input latitude, output latitude, time, energy.
-            
+            N_E = np.array(N_E).swapaxes(1,3)
+            S_E = np.array(S_E).swapaxes(1,3)
+#            print np.shape(N_E)
+            self.E_bands = np.linspace(1,np.shape(N_E)[2], np.shape(N_E)[2])
+#            print self.E_bands
+            self.N_E_interp = interpolate.RegularGridInterpolator((in_lats, obj.coords.lat(), self.E_bands, obj.t), N_E, fill_value=0,bounds_error=False)
+            self.S_E_interp = interpolate.RegularGridInterpolator((in_lats, obj.coords.lat(), self.E_bands, obj.t), S_E, fill_value=0,bounds_error=False)
+
 
     def get_precip_at(self, in_lat, out_lat, t, hemisphere = "N"):
         ''' in_lat:  Flash latitude (degrees)
@@ -62,8 +75,20 @@ class precip_model(object):
         else:
             return self.S_interp(inps)
 
-    def tile_keys(self,key1, key2):
 
+    def get_precip_at(self, in_lat, out_lat, energy, t, hemisphere = "N"):
+        if not self.multiple_bands:
+            print "No multi-band!"
+        else:
+            inps = self.tile_keys(np.array([in_lat, out_lat, energy]), t)
+
+        if hemisphere=="N":
+            return self.N_E_interp(inps)
+        else:
+            return self.N_E_interp(inps)
+
+
+    def tile_keys(self, key1, key2):
         return np.vstack([np.outer(key1, np.ones(np.size(key2))), key2]).T
 
     def get_longitude_scaling(self, inp_lat, inp_lon, out_lon, I0=None, db_scaling = False):
@@ -86,22 +111,30 @@ class precip_model(object):
 
 if __name__ =="__main__":
 
-    m = precip_model("database_multiple.pkl")
+    m = precip_model("database_counts.pkl",multiple_bands=True)
 
-    t = np.linspace(0,30,100)
-    out_lats = np.linspace(30,70,60)
-    in_lat = 19
-    res = []
+    t = np.linspace(0,30,300)
+    tmp = m.get_precip_at(30,45,5,t,"N")
 
-    for o in out_lats:
-        points = m.tile_keys((in_lat, o), t)
-        res.append(m.N_interp(points))
-
-    res = np.log10(np.array(res))
-
-    plt.pcolor(t, out_lats, res)
-    plt.clim([-4,4])
+    plt.plot(t,tmp)
     plt.show()
+    #tmp = [m.get_precip_at(30,45,x,t,"N") for x in m.E_bands]
+    #print tmp
+
+    # t = np.linspace(0,30,100)
+    # out_lats = np.linspace(30,70,60)
+    # in_lat = 19
+    # res = []
+
+    # for o in out_lats:
+    #     points = m.tile_keys((in_lat, o), t)
+    #     res.append(m.N_interp(points))
+
+    # res = np.log10(np.array(res))
+
+    # plt.pcolor(t, out_lats, res)
+    # plt.clim([-4,4])
+    # plt.show()
 
 
 
